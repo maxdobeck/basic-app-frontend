39272101787a5937f274ebd4a8520d7bec6be53d
Max Dobeck <dobeckmax@gmail.com>
2018-02-16 17:47:44 +0100

    Basic Home Page
    Basic Homepage and Nav bar added.

---
+++ test/unit/specs/Home.spec.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import Home from '@/components/Home'
+
+describe('Home.vue', () => {
+  it('should render correct contents', () => {
+    const Constructor = Vue.extend(Home)
+    const vm = new Constructor().$mount()
+  })
+})
