c30f2ac1493d6fc2972a5dcfea4e46bfacf31317
Max Dobeck <dobeckmax@gmail.com>
2018-04-27 23:11:33 -0700

    Changed styling and settings of page widths.

--- src/App.vue
+++ src/App.vue
@@ -3,23 +3,22 @@
     <v-toolbar
     flat
     app
+    id="navbar"
     >
       <v-toolbar-items>
-        <v-btn :ripple="false" flat to="/">Home</v-btn>
-        <v-btn :ripple="false" flat to="/schedules" v-if="loggedIn">Schedules</v-btn>
+        <v-btn :ripple="false" flat to="/" id="home" >Home</v-btn>
+        <v-btn :ripple="false" flat to="/schedules" v-if="loggedIn" id="schedules">Schedules</v-btn>
       </v-toolbar-items>
       <v-spacer></v-spacer>
       <v-toolbar-items>
-        <v-btn :ripple="false" flat raised to="/signup" v-if="!loggedIn">Sign Up</v-btn>
-        <v-btn :ripple="false" flat raised to="/login" v-if="!loggedIn">Log In</v-btn>
-        <v-btn :ripple="false" flat raised to="/logout" v-if="loggedIn">Log Out</v-btn>
+        <v-btn :ripple="false" flat raised to="/signup" v-if="!loggedIn" id="signup">Sign Up</v-btn>
+        <v-btn :ripple="false" flat raised to="/login" v-if="!loggedIn" id="logInOut">Log In</v-btn>
+        <v-btn :ripple="false" flat raised to="/logout" v-if="loggedIn" id="logInOut">Log Out</v-btn>
       </v-toolbar-items>
     </v-toolbar>
 
     <v-content>
-      <v-container>
         <router-view></router-view>
-      </v-container>
     </v-content>
   </v-app>
 </template>
@@ -36,4 +35,16 @@ export default {
 </script>
 
 <style scoped>
+  #home {
+    margin-left: 5%;
+  }
+  #schedules {
+    margin-left: 5%;
+  }
+  #signup {
+    margin-right: 5%;
+  }
+  #logInOut {
+    margin-right: 5%;
+  }
 </style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,13 @@
 <template>
   <v-container>
-    <h1>Scheduling Is Hard</h1>
+    <v-layout row wrap>
+      <v-flex xl12>
+        <h1 id="homeTitle">Scheduling Is Hard</h1>
+      </v-flex>
+      <v-flex>
+        <h2 id="homeSub">Let us help!</h2>
+      </v-flex>
+    </v-layout>
   </v-container>
 </template>
 
