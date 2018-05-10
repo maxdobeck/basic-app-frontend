7873f6e33c7cb30276caa19309107bde708cfbd1
Max Dobeck <msdobeck@gmail.com>
2018-05-09 22:09:04 -0700

    Heroku deploy was missing the express package. Server needed the const instead of variables.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-var express = require('express');
-var app = express();
-var path = require('path');
+const express = require('express');
+const app = express();
+const path = require('path');
 app.use(express.static(path.join(__dirname, 'dist')))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
