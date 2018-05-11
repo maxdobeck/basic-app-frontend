96f907019739ff8d8941d98192ae7079487d57e1
Max Dobeck <msdobeck@gmail.com>
2018-05-11 10:54:15 -0700

    Removed all dev dependencies from server.js

--- server.js
+++ server.js
@@ -3,11 +3,5 @@ const app = express();
 const path = require('path');
 app.use(express.static(path.join(__dirname, 'dist')))
 
-const config = require('./config')
-if (!process.env.NODE_ENV) {
-  process.env.NODE_ENV = JSON.parse(config.dev.env.NODE_ENV)
-}
-
-// default port where dev server listens for incoming traffic
-const port = process.env.PORT || config.dev.port
+const port = process.env.PORT
 app.listen(port, () => console.log('Example app listening on port', port))
