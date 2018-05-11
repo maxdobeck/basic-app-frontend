f6f492268cf60d244f4fca1104be28052bc47837
Max Dobeck <msdobeck@gmail.com>
2018-05-11 11:02:10 -0700

    changed language of server startup output. Added heroku specific postbuild step.

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const port = process.env.PORT
+
 app.use(express.static(path.join(__dirname, 'dist')))
 
-const port = process.env.PORT
-app.listen(port, () => console.log('Example app listening on port', port))
+app.listen(port, () => console.log('Listening on port', port))
