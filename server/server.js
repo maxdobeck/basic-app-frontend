26e4e0944d8e6467754860aa09d8619da90d33e4
Max Dobeck <msdobeck@gmail.com>
2018-05-20 11:43:13 -0700

    Tested that api server can validate commands from URI values.

--- server/server.js
+++ server/server.js
@@ -2,25 +2,22 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const {check, validationResult} = require('express-validator/check')
-const port = process.env.PORT
+const port = 3131// process.env.PORT
 
 app.use(express.static(path.join(__dirname, 'dist')))
 
-app.get("/validate/signup",/* [
+app.get("/validate/signup", [
   check('name').isAlphanumeric(),
   check('email').isEmail(),
-  check('password').isLength({ min: 10 }),
+  check('password').isLength({ min:10 })
   ], function(req, res) {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(422).json({ errors: errors.array() });
+      return res.status(422).json({ errors: errors.array() })
     }
     else {
-      return res.status(200)
-    }*/
-  function(req,res) {
-    console.log(req.query)
-    res.json({ message: 'Hello' })
+      return res.status(200).json({ errors: errors.array() })
+    }
 })
 
 app.listen(port, () => console.log('Listening on port', port))
