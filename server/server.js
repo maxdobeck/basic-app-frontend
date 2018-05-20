23a33716b677ab3ba75e6bfab6fd706a4552f6f7
Max Dobeck <msdobeck@gmail.com>
2018-05-19 22:44:27 -0700

    return 200 if everything is OK

--- server/server.js
+++ server/server.js
@@ -7,13 +7,16 @@ const port = process.env.PORT
 app.use(express.static(path.join(__dirname, 'dist')))
 
 app.post("/validate/signup", [
-  check('name').isEmpty(),
+  check('name').isAlphanumeric(),
   check('email').isEmail(),
   check('password').isLength({ min: 15 }),
   ], function(req, res) {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(422).json({ errors: errors.array() });
+    }
+    else {
+      return res.status(200)
     }
 })
 
