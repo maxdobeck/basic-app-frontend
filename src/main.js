76544b09028b8ced75a442339439408e5f851dfc
Max Dobeck <msdobeck@gmail.com>
2018-05-22 14:53:05 -0700

    adding back source files.

--- src/main.js
+++ src/main.js
@@ -3,12 +3,18 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import store from '../store'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+
+Vue.use(Vuetify)
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  store,
   router,
   components: { App },
   template: '<App/>'
