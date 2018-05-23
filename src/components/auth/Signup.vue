0a4c1769b31068eef6e9ef784cd0c56e3c26216b
Max Dobeck <msdobeck@gmail.com>
2018-05-22 17:06:06 -0700

    Password visibility logic working but not importing icon.

--- src/components/auth/Signup.vue
+++ src/components/auth/Signup.vue
@@ -1,11 +1,11 @@
 <template>
   <v-stepper v-model="el" id="stepper">
     <v-stepper-header>
-      <v-stepper-step step="1" editable :complete="el > 1">Name</v-stepper-step>
+      <v-stepper-step step="1" editable>Name</v-stepper-step>
       <v-divider></v-divider>
-      <v-stepper-step step="2" editable :complete="el > 2">Email</v-stepper-step>
+      <v-stepper-step step="2" editable>Email</v-stepper-step>
       <v-divider></v-divider>
-      <v-stepper-step step="3" editable :complete="el > 3">Password</v-stepper-step>
+      <v-stepper-step step="3" editable>Password</v-stepper-step>
     </v-stepper-header>
     <v-stepper-items>
       <v-stepper-content step="1">
@@ -29,7 +29,7 @@
 
       <v-stepper-content step="2">
         <v-card height="300px">
-          <v-form v-model="validEmail" ref="form" lazy-validation v-on:submit.prevent>
+          <v-form v-model="validEmail" ref="form" v-on:submit.prevent>
             <v-layout row justify-center>
               <v-flex xs8>
                 <v-text-field
@@ -65,9 +65,9 @@
                   :rules="passRules"
                   :counter="10"
                   required
-                  :append-icon="p1 ? 'visibility' : 'visibility_off'"
-                  :append-icon-cb="() => (p1 = !p1)"
-                  :type="p1 ? 'password' : 'text'"
+                  :append-icon="pass1Visible ? 'visibility' : 'visibility_off'"
+                  :append-icon-cb="() => (pass1Visible = !pass1Visible)"
+                  :type="pass1Visible ? 'password' : 'text'"
                 >
                 </v-text-field>
                 <v-text-field
@@ -76,9 +76,9 @@
                   :rules="passConfirmRules"
                   :counter="10"
                   required
-                  :append-icon="p2 ? 'visibility' : 'visibility_off'"
-                  :append-icon-cb="() => (p2 = !p2)"
-                  :type="p2 ? 'password' : 'text'"
+                  :append-icon="pass2Visible ? 'visibility' : 'visibility_off'"
+                  :append-icon-cb="() => (pass2Visible = !pass2Visible)"
+                  :type="pass2Visible ? 'password' : 'text'"
                   >
                 </v-text-field>
                 <v-alert v-model="badDataAlert" type="error">
@@ -102,8 +102,8 @@ export default {
   data () {
     return {
       el: 0,
-      p1: true,
-      p2: true,
+      pass1Visible: true,
+      pass2Visible: true,
       validName: false,
       name: '',
       nameRules: [
