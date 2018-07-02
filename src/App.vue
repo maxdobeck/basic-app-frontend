2d44542718d29d96a1105af7c5b84cd335a0a5b6
Max Dobeck <msdobeck@gmail.com>
2018-07-01 19:53:52 -0700

    Updated the api allocation method.

--- src/App.vue
+++ src/App.vue
@@ -30,10 +30,10 @@
 <script>
 import { mapGetters } from 'vuex'
 let api // Need to find a way to turn all this into a function
-if (process.env.NODE_ENV === 'test') {
-  api = process.env.TEST_API
-} else if (process.env.NODE_ENV === 'dev') {
-  api = process.env.DEV_API
+if (process.env.NODE_ENV === 'dev') {
+  api = 'http://localhost:3000/'
+} else {
+  api = 'https://shielded-stream-75107.herokuapp.com/'
 }
 const apiURL = api + '/csrftoken'
 export default {
