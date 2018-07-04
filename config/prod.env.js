ed28824ca577d0d5a4b70a33d5599a472c5650e6
Max Dobeck <dobeckmax@gmail.com>
2018-07-04 12:40:56 -0700

    Add Dev and Prod Configs + Deploys (#22)
    * removed incorrect url from readme.
    
    * Loading buttons (#14)
    
    * Added loading button to login route.
    
    * Added loading buttons and fixed issue with error[] not getting the api responses.
    
    * updated prod travis.
    
    * Testing if s3 deploy will respect the config directory after webpack build.
    
    * moved configs from dev.env to prod.env.
    
    * starting the process of adding a buildTest option for heroku test env.
    
    * Added new files for buildTest command.
    
    * Added all files necessary for 'yarn buildTest' command.
    
    * Updated travis to use correct build pattern.
    
    * Config for Test Environment (#21)
    
    * starting the process of adding a buildTest option for heroku test env.
    
    * Added new files for buildTest command.
    
    * Added all files necessary for 'yarn buildTest' command.
    
    * Updated travis to use correct build pattern.
    
    * Added if statment to reinforce and hopefully fail faster if not true.
    
    * Updated prod configs.

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,6 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  PROD_API: '"https://rugged-wind-cave-81042.herokuapp.com/"',
+  MY_TEST: '"My big test case"'
 }
